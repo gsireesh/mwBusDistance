416dbd5dc42cff8a92502ae90c2b01c392a600c2
Sireesh Gururaja <skg2123@columbia.edu>
2014-06-21 10:08:30 -0400

    Gets image for first bus, displays with Tkinter.

--- mwBusDistance.py
+++ mwBusDistance.py
@@ -10,27 +10,44 @@ Resources used:
 
 Adapted from: 
     http://hci574.blogspot.com/2010/04/using-google-maps-static-images.html
+
+Uses BeautifulSoup for html and xml parsing, and pillow to display the images.
 """
 
 import sys
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
-#from Tkinter import *
-#import Image, ImageTk
+from io import BytesIO
+from tkinter import *
+from PIL import Image
+from PIL import ImageTk
 
-import re
 
-ROUTE = 'RT01'
+class MapFrame(Frame):
+  def __init__(self, master, im):
+    Frame.__init__(self, master)
+    self.caption = Label(self, text='Distance Map')
+    self.caption.grid()
+    self.image = ImageTk.PhotoImage(im)
+    self.image_label = Label(self, image=self.image, bd=0)
+    self.image_label.grid()
+    self.grid()
+
+ROUTE = 'RT07'
 TABLE_ID = 'Table1'
+fromFile = False
 
-mText = urlopen('http://www.geolabvirtualmaps.com/pda/metrowest.aspx')\
-.read().decode('utf-8')
+if(fromFile):
+  mText = urlopen('http://www.geolabvirtualmaps.com/pda/metrowest.aspx')\
+  .read().decode('utf-8')
+else:
+  mText = open('mwrta.html').read()
 
 table = BeautifulSoup(mText).find(id=TABLE_ID)
 buses = []
 
 #need to get rid of tbody
-for i in table.tbody.children:
+for i in table.children:
   if( i == '\n'): continue
   if(i.td.string == ROUTE ):
     row = [cell.string for cell in i.children if cell != '\n']
@@ -40,6 +57,14 @@ for i in table.tbody.children:
 
 
 request = '''http://maps.googleapis.com/maps/api/staticmap?center=%22Mathworks+\
-Natick,MA%22&size=640x640&markers=color:red|Mathworks&markers=color:green|{}'''\
+Natick,MA%22&size=500x500&markers=color:red|Mathworks&markers=color:green|{}'''\
 .format(buses[0][0])
+
+imData = urlopen(request).read()
+image = Image.open(BytesIO(imData))
+
+mainw = Tk()
+mainw.frame = MapFrame(mainw, image)
+mainw.mainloop()
+
 
